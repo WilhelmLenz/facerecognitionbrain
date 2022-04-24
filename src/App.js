c48601f9173197b1063430fcf5c430059a9de396
WilhelmLenz <wilhelm.lenz@neteleven.de>
2022-04-24 12:32:31 +0200

    added new empty files for css and script

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation'
+// import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+// import Logo from './components/Logo/Logo'
+// import Facerecognition from './components/Facerecognition/Facerecognition'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Navigation />
+       {/* <Logo />
+       <ImageLinkForm />
+       <Facerecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navigation.css";
+
+
+const Navigation = () => {
+    return (
+      <nav>
+        <p>Sign Out</p>
+      </nav>
+    )
+}
+
+
+export default Navigation;

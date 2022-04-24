96702389490db15abdd264e34d1db59b60dcb2e9
WilhelmLenz <wilhelm.lenz@neteleven.de>
2022-04-24 21:16:44 +0200

    added particles to background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
+import React, { useCallback } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import particlesOptions from "./particles.json";
 // import Facerecognition from './components/Facerecognition/Facerecognition'
 import './App.css';
 
 
-class App extends Component {
-  render() {
+function App() {
+  const particlesInit = useCallback(main => {
+    loadFull(main);
+  }, [])
+
     return (
       <div className="App">
-       <Navigation />
-       <Logo />
-       <Rank />
-       <ImageLinkForm />
+        <Particles options={particlesOptions} init={particlesInit}/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
        {/* <Facerecognition />  */}
       </div>
     );
   }
-}
 
 export default App;
